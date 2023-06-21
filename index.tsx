9a703d890a5e2d5c6ba2fa473c232a8ee4e2f469
Samuel G <samuel.goncalves@epita.fr>
2023-06-21 03:12:36 +0200

    initial

---
+++ index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Ma page web</h1>
+      <p>Bienvenue sur ma page web !</p>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
